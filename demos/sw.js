4d862d59caf56ab184d5ab431576b0f60658d816
Matt Giuca <mgiuca@chromium.org>
2017-09-01 15:48:09 +1000

    demo: Register a dummy service worker.

---
+++ demos/sw.js
@@ -0,0 +1,6 @@
+/* This work is licensed under the W3C Software and Document License
+ * (http://www.w3.org/Consortium/Legal/2015/copyright-software-and-document).
+ */
+
+self.addEventListener('fetch', function(event) {
+});
