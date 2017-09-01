a3af5514ea3090126ee5fbc655f71b65e4c227af
Matt Giuca <mgiuca@chromium.org>
2017-09-01 16:07:49 +1000

    demo: sw fetch returns 200 even when offline.

--- demos/sw.js
+++ demos/sw.js
@@ -2,5 +2,14 @@
  * (http://www.w3.org/Consortium/Legal/2015/copyright-software-and-document).
  */
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
+  event.respondWith((async () => {
+    // Get content from the network.
+    try {
+      return await fetch(event.request);
+    } catch (e) {
+      // Failure. Just return a 200 page, to satisfy Lighthouse.
+      return new Response('You are offline :(', {status: 200});
+    }
+  })());
 });
